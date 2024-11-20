064052c05cf6530f05e16e8ba5239ee3322414f2
qijian <wanqijian@outlook.com>
2024-11-20 10:54:06 +0800

    add route extract

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use axum::extract::Path;
 use axum::{response::Html, routing::get, Router};
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(handler));
+    let app = Router::new()
+        .route("/", get(handler))
+        .route("/book/:id", get(path_extract));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
@@ -13,4 +16,8 @@ async fn main() {
 
 async fn handler() -> Html<&'static str> {
     Html("<h1>Hello, World!</h1>")
+}
+
+async fn path_extract(Path(id): Path<u32>) -> Html<String> {
+    Html(format!("Book ID: {}", id))
 }
