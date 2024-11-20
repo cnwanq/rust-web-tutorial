a0a66acff4fb809c3200f074a013b2dfa02395f6
qijian <wanqijian@outlook.com>
2024-11-20 11:15:45 +0800

    add request query extract

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use axum::extract::Path;
+use std::collections::HashMap;
+
+use axum::extract::{Path, Query};
 use axum::{response::Html, routing::get, Router};
 
 #[tokio::main]
@@ -6,7 +8,8 @@ async fn main() {
     // build our application with a single route
     let app = Router::new()
         .route("/", get(handler))
-        .route("/book/:id", get(path_extract));
+        .route("/book/:id", get(path_extract))
+        .route("/book", get(query_extract));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
@@ -14,10 +17,16 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
+// 路由处理函数
 async fn handler() -> Html<&'static str> {
     Html("<h1>Hello, World!</h1>")
 }
 
+// 添加路由参数提取功能
 async fn path_extract(Path(id): Path<u32>) -> Html<String> {
     Html(format!("Book ID: {}", id))
+}
+
+async fn query_extract(Query(params): Query<HashMap<String, String>>) -> Html<String> {
+    Html(format!("{:?}", params))
 }
